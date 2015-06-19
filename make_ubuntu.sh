893da20459e0b9ebfff66c02eae2063562d11bd1
Toomore Chiang <toomore0929@gmail.com>
2015-06-19 20:02:15 +0000

    Tiny changed.

--- make_ubuntu.sh
+++ make_ubuntu.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 cp ~/.vim/toomore.vim/vimrc.ubuntu ~/.vimrc
 vim +PluginInstall +qall
+vim +GoInstallBinaries +qall
 cp -r ~/.vim/toomore.vim/plugin ~/.vim/
 curl https://raw.githubusercontent.com/fatih/molokai/master/colors/molokai.vim --create-dirs -o ~/.vim/colors/molokai_toomore.vim
