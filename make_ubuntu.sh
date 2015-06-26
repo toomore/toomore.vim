543b0eabc3a4737ace965861e5147943a7a702dc
Toomore Chiang <toomore0929@gmail.com>
2015-06-26 06:46:24 +0000

    Direct pull vim plugin.

--- make_ubuntu.sh
+++ make_ubuntu.sh
@@ -1,5 +1,32 @@
 #!/bin/bash
 cp ~/.vim/toomore.vim/vimrc.ubuntu ~/.vimrc
+mkdir -p ~/.vim/bundle
+cd  ~/.vim/bundle
+git clone --depth 1 https://github.com/gmarik/Vundle.vim.git
+git clone --depth 1 https://github.com/tpope/vim-fugitive.git
+git clone --depth 1 https://github.com/L9.git
+git clone --depth 1 https://github.com/Lokaltog/vim-easymotion.git
+git clone --depth 1 https://github.com/Shougo/neocomplete.vim.git
+git clone --depth 1 https://github.com/ap/vim-css-color.git
+git clone --depth 1 https://github.com/bling/vim-airline.git
+git clone --depth 1 https://github.com/chase/vim-ansible-yaml.git
+git clone --depth 1 https://github.com/ekalinin/Dockerfile.vim.git
+git clone --depth 1 https://github.com/fatih/molokai.git
+git clone --depth 1 https://github.com/fatih/vim-go.git
+git clone --depth 1 https://github.com/jelera/vim-javascript-syntax.git
+git clone --depth 1 https://github.com/jstemmer/gotags.git
+git clone --depth 1 https://github.com/lepture/vim-jinja.git
+git clone --depth 1 https://github.com/majutsushi/tagbar.git
+git clone --depth 1 https://github.com/othree/html5-syntax.vim.git
+git clone --depth 1 https://github.com/othree/html5.vim.git
+git clone --depth 1 https://github.com/scrooloose/syntastic.git
+git clone --depth 1 https://github.com/wavded/vim-stylus.git
+git clone --depth 1 https://github.com/vim-scripts/AutoComplPop.git
+git clone --depth 1 https://github.com/vim-scripts/mako.vim.git
+git clone --depth 1 https://github.com/vim-scripts/nginx.vim.git
+git clone --depth 1 https://github.com/vim-scripts/python.vim.git
+git clone --depth 1 https://github.com/vim-scripts/python_match.vim.git
+git clone --depth 1 git://git.wincent.com/command-t.git
 vim +PluginInstall +qall
 vim +GoInstallBinaries +qall
 cp -r ~/.vim/toomore.vim/plugin ~/.vim/
