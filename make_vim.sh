a380474a76cabadd4959f3046f828a54b697e1a2
Toomore Chiang <toomore0929@gmail.com>
2016-10-03 15:20:26 +0800

    Update vim to 8.

--- make_vim.sh
+++ make_vim.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-VIMVER="7.4.1699"
+VIMVER="8.0.0021"
 
 apt-get update && apt-get dist-upgrade -y && \
 apt-get install -y git curl gcc tmux \
-                       make ncurses-dev libtolua-dev exuberant-ctags && \
+                   make ncurses-dev libtolua-dev exuberant-ctags     && \
     ln -s /usr/include/lua5.2/ /usr/include/lua                      && \
     ln -s /usr/lib/x86_64-linux-gnu/liblua5.2.so /usr/lib/liblua.so  && \
 
@@ -14,5 +14,5 @@ cd ./vim-${VIMVER}/
 ./configure --with-features=huge --enable-luainterp --enable-multibyte \
             --with-tlib=ncurses --enable-cscope                        \
             --enable-gui=no --without-x --prefix=/usr               && \
-make VIMRUNTIMEDIR=/usr/share/vim/vim74                             && \
+make VIMRUNTIMEDIR=/usr/share/vim/vim80                             && \
 make install
